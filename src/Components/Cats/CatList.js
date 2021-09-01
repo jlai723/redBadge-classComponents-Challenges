5c3fb4904af4605dfa15b140d4f6a2abc966d28b
Jackie Lai <jlai@bbbsci.org>
2021-08-31 21:48:36 -0400

    Completed Cats challenge

--- src/Components/Cats/CatList.js
+++ src/Components/Cats/CatList.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
- 
-const CatList = () => {
- return (
-   <div>
-     {props.breeds.Map(cat => <li>cat</li> )}
-   </div>
- )
+
+class CatList extends Component {
+  render() {
+    return (
+      <div>
+        <li>{ this.props.breed }</li>
+        {/* {this.props.breeds.map(cat => <li>{cat}</li>)} */}
+      </div>
+    )
+  }
 }
- 
-CatList;
+
+  export default CatList;
